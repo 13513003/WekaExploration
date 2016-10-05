aa20a9d116b7adf0df957ad3793346d5fb55a50f
Juan Anton <juan.anton.che@gmail.com>
2016-10-05 13:01:03 +0700

    Added handleMissingValues, computeGainRatio, splitData

--- DecisionTree/src/decisiontree/MyC45.java
+++ DecisionTree/src/decisiontree/MyC45.java
@@ -21,6 +21,7 @@ import weka.core.TechnicalInformation.Field;
 import weka.core.TechnicalInformation.Type;
 
 import java.util.Enumeration;
+import weka.core.AttributeStats;
 
 /**
  *
@@ -82,7 +83,6 @@ public class MyC45 extends Classifier {
      return result;
     }
     
-    // TODO: Handle Missing Values
     // TODO: PruneTree
     // TODO: Calculate Gain Ratio
     // TODO: Handle Numeric and Nominal Values
@@ -90,11 +90,14 @@ public class MyC45 extends Classifier {
         // can classifier handle the data?
         getCapabilities().testWithFail(instances);
 
-        // remove instances with missing class
+        // handle instances with missing class
         instances = new Instances(instances);
         instances.deleteWithMissingClass();
+        
+        // handle missing values
+        Instances processedInstances = handleMissingValues(instances);
 
-        makeTree(instances);
+        makeTree(processedInstances);
     }
     
     /**
@@ -113,18 +116,19 @@ public class MyC45 extends Classifier {
        return;
      }
 
-     // Compute attribute with maximum information gain.
-     double[] infoGains = new double[instances.numAttributes()];
-     Enumeration attEnum = instances.enumerateAttributes();
-     while (attEnum.hasMoreElements()) {
-       Attribute att = (Attribute) attEnum.nextElement();
-       infoGains[att.index()] = computeInfoGain(instances, att);
+     // Compute attribute with maximum gain ratio.
+     double[] gainRatios = new double[instances.numAttributes()];
+     Enumeration attrEnum = instances.enumerateAttributes();
+     while (attrEnum.hasMoreElements()) {
+       Attribute attr = (Attribute) attrEnum.nextElement();
+       // TODO: 2 types of computeGainRatio to handle numeric and nominal values
+       gainRatios[attr.index()] = computeGainRatio(instances, attr);
      }
-     m_Attribute = instances.attribute(Utils.maxIndex(infoGains));
+     m_Attribute = instances.attribute(Utils.maxIndex(gainRatios));
 
-     // Make leaf if information gain is zero. 
+     // Make leaf if gain ratio is zero. 
      // Otherwise create successors.
-     if (Utils.eq(infoGains[m_Attribute.index()], 0)) {
+     if (Utils.eq(gainRatios[m_Attribute.index()], 0)) {
        m_Attribute = null;
        m_Distribution = new double[instances.numClasses()];
        Enumeration instEnum = instances.enumerateInstances();
@@ -143,5 +147,108 @@ public class MyC45 extends Classifier {
          m_Successors[j].makeTree(splitData[j]);
        }
      }
+    }
+    
+    /**
+    * Splits a dataset according to the values of a nominal attribute.
+    *
+    * @param data the data which is to be split
+    * @param att the attribute to be used for splitting
+    * @return the sets of instances produced by the split
+    */
+    private Instances[] splitData(Instances data, Attribute att) {
+
+     Instances[] splitData = new Instances[att.numValues()];
+     for (int j = 0; j < att.numValues(); j++) {
+       splitData[j] = new Instances(data, data.numInstances());
+     }
+     Enumeration instEnum = data.enumerateInstances();
+     while (instEnum.hasMoreElements()) {
+       Instance inst = (Instance) instEnum.nextElement();
+       splitData[(int) inst.value(att)].add(inst);
+     }
+     for (int i = 0; i < splitData.length; i++) {
+       splitData[i].compactify();
+     }
+     return splitData;
+    }
+    
+    /**
+    * Computes information gain for an attribute.
+    *
+    * @param data the data for which info gain is to be computed
+    * @param att the attribute
+    * @return the information gain for the given attribute and data
+    * @throws Exception if computation fails
+    */
+    private double computeGainRatio(Instances instances, Attribute attr) throws Exception {
+        double gainRatio = computeEntropy(instances);
+        Instances[] splitData = splitData(instances, attr);
+        for (int j = 0; j < attr.numValues(); j++) {
+          if (splitData[j].numInstances() > 0) {
+            gainRatio -= ((double) splitData[j].numInstances() /
+                         (double) instances.numInstances()) *
+              computeEntropy(splitData[j]);
+          }
+        }
+        return gainRatio;
+    }
+    
+    /**
+    * Computes the entropy of a dataset.
+    * 
+    * @param data the data for which entropy is to be computed
+    * @return the entropy of the data's class distribution
+    * @throws Exception if computation fails
+    */
+    private double computeEntropy(Instances data) throws Exception {
+
+     double [] classCounts = new double[data.numClasses()];
+     Enumeration instEnum = data.enumerateInstances();
+     while (instEnum.hasMoreElements()) {
+       Instance inst = (Instance) instEnum.nextElement();
+       classCounts[(int) inst.classValue()]++;
+     }
+     double entropy = 0;
+     for (int j = 0; j < data.numClasses(); j++) {
+       if (classCounts[j] > 0) {
+         entropy -= classCounts[j] * Utils.log2(classCounts[j]);
+       }
+     }
+     entropy /= (double) data.numInstances();
+     return entropy + Utils.log2(data.numInstances());
+    }
+    
+    private Instances handleMissingValues(Instances data) {
+        Instances newData = data;
+        Enumeration attrEnum = newData.enumerateAttributes();
+        while (attrEnum.hasMoreElements()) {
+            Attribute attr = (Attribute) attrEnum.nextElement();
+            if (attr.isNominal()) {
+                int maxIdx = 0;
+                AttributeStats attrStats = newData.attributeStats(attr.index());
+                for (int i=0; i<attr.numValues(); i++) {
+                    if (attrStats.nominalCounts[i] > attrStats.nominalCounts[maxIdx]) {
+                        maxIdx = i;
+                    }
+                }
+
+                for (int i=0; i<newData.numInstances(); i++) {
+                    if (newData.instance(i).isMissing(attr.index())) {
+                        newData.instance(i).setValue(attr.index(), maxIdx);
+                    }
+                }
+            }
+            else if (attr.isNumeric()) {
+                double mean = newData.attributeStats(attr.index()).numericStats.mean;
+                for (int i=0; i<newData.numInstances(); i++) {
+                    if (newData.instance(i).isMissing(attr.index())) {
+                        newData.instance(i).setValue(attr.index(), mean);
+                    }
+                }
+            }
+        }
+
+        return newData;
     }
 }
